e2d26b1c4e1ed6fb37ad64ab79973232633a3b80
mateus.macedo <mhbassomacedo@gmail.com>
2017-11-21 10:49:29 -0200

    Try to put the javaLibrary into backendLibrary but still not work
    Signed-off-by: mateus.macedo <mhbassomacedo@gmail.com>

---
+++ jokelibrary/src/main/java/com/udacity/gradle/builditbigger/JokeFactory.java
@@ -0,0 +1,32 @@
+package com.example.android.jokelibrary;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+/**
+ * Created by lsitec101.macedo on 20/11/17.
+ */
+
+public class JokeFactory {
+
+    private ArrayList<String> jokes;
+
+    public JokeFactory() {
+        jokes.add("Do que o diabo morreu? De diabetes");
+        jokes.add("O que fala o livro de Matemática para o livro de História? Não me venha com história que eu já estou cheio de problemas");
+        jokes.add("Porque o louco toma banho com o chuveiro desligado? Por que ele comprou um shampoo para cabelos secos");
+        jokes.add("Porque a roda de trem é de ferro e o não de borracha? Por que se fosse de borracha apagaria a linha");
+        jokes.add("Qual estado do Brasil que queria ser carro? Sergipe");
+        jokes.add("Qual o único pais que não tem padaria? O Jápão");
+    }
+
+    public String returnOneJoke() {
+        int position  = new Random().nextInt(jokes.size() - 1);
+        return jokes.get(position);
+    }
+
+    public ArrayList<String> returnAllJokes() {
+        return jokes;
+    }
+
+}
